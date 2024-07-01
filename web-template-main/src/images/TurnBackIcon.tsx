ee13d07e6ea5d9ebe1e3009912996596ec745663
Adelya Ahmadieva <adelaahmadieva@gmail.com>
2024-07-02 01:20:14 +0300

    styles cleaning

--- web-template-main/src/images/TurnBackIcon.tsx
+++ web-template-main/src/images/TurnBackIcon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TurnBackIcon = () => {
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_6_954)">
+            <g clipPath="url(#clip0_6_954)">
                 <path d="M10 9V5L3 12L10 19V14.9C15 14.9 18.5 16.5 21 20C20 15 17 10 10 9Z" fill="#A1A1AA"/>
             </g>
             <defs>
